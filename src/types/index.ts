3354bb2265725b88f71d39401e379b06a8f30bc1
Stepan-Suprunov <ipsuprunovso@mail.ru>
2025-03-27 18:12:43 +0300

    API responses types initialized

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+export type PostType = {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+};
+
+export type UserType = {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: {
+            lat: string;
+            lng: string;
+        };
+    };
+};
+
+export type CommentType = {
+    postId: number;
+    id: number;
+    name: string;
+    email: string;
+    body: string;
+};
